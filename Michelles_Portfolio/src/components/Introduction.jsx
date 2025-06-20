49fab7da77dbd0687a966eb04d106b2b5db08d98
Michelle Wee <michellew1822@gmail.com>
2025-06-20 02:37:20 -0700

    fixed styling

--- Michelles_Portfolio/src/components/Introduction.jsx
+++ Michelles_Portfolio/src/components/Introduction.jsx
@@ -12,11 +12,11 @@ function Introduction() {
         </h1>
         <p className = "introText">
             Hi, I'm Michelle, an incoming fourth-year attending the University of California San Diego, majoring in Computer Science with a 
-            minor in Cognitive Science.I am passionate about building intelligent software solutions and have gained hands-on experience in 
+            minor in Cognitive Science. I am passionate about building intelligent software solutions and have gained hands-on experience in 
             software development and machine learning throughout my time in college. In the future, I hope to contribute to innovative software 
             solutions that harness the power of AI and machine learning to improve people’s lives and to impact the world.
         </p>
-        <a className = "resumeButton" href="/resume.pdf" download>Download My Resume</a>
+        <a className = "resumeButton" href="https://docs.google.com/document/d/1yxwJvILbZGjwvqFF6PGSkCUXcnz6qH2YGJsIg52HeIo/edit?tab=t.0" target="_blank" rel="noopener noreferrer"> View My Resume </a>
     </div>
 
   )
--- Michelles_Portfolio/src/components/Projects.jsx
+++ Michelles_Portfolio/src/components/Projects.jsx
@@ -7,64 +7,66 @@ function Projects() {
     const [ArcadeMediaFlag, setArcadeMediaFlag] = useState(false);
 
   return (
-    <div className = "">
+    <div>
         <h1 className = "subTitle">
             My Projects:
         </h1>
         <div className = "projectOuterContainer">
 
-            <div className = "projectContainer">
-                <h1 className = "projectTitle">
-                    Arcade
-                </h1>
-                <p className = "projectText">
-                    - Developed an interactive web-based game using React (JavaScript) and CSS. <br/>
-                    - Controlled animation flow by syncing React component state with gameplay logic to trigger re-renders and animate game elements in response to user actions. <br/>
-                    - Designed an engaging user interface with custom visual assets, CSS animations, background music, and sound effects to enhance the player experience. <br/>
-                    - Implemented game logic and dynamic outcomes based on user input, with real-time feedback. <br/>
-                    - Developed a macOs desktop application using Tauri.<br/>
-                </p>
-                <h1 className = "projectText">
-                    Media:
-                </h1>
-                <div className = "projectMediaContainer">
-                    <a className = "projectButton" href="https://github.com/MichelleW1234/Arcade.git" target="_blank" rel="noopener noreferrer"> GitHub Repository </a>
-                    <a className = "projectButton" href="https://github.com/MichelleW1234/Arcade/releases/download/v1.0.0/Arcade_1.0.0.dmg"> Download Arcade 1.0.0</a>
-                    <button className = "projectButton" onClick = {() => setArcadeMediaFlag(true)}> View Video Content </button>
+            <div className = "projectInnerContainer">
+
+                <div className = "projectContainer">
+                    <h1 className = "projectTitle">
+                        Arcade
+                    </h1>
+                    <p className = "projectText">
+                        - A retro-style browser game with interactive mini-games and dynamic scoring. <br/>
+                        - Used React, Vite, and Router DOM for logic, state, and animation control. <br/>
+                        - Designed custom visuals, sound, and packaged as macOS desktop app with Tauri. <br/>
+                    </p>
+                    <h1 className = "projectText">
+                        Media:
+                    </h1>
+                    <div className = "projectMediaContainer">
+                        <a className = "projectButton" href="https://github.com/MichelleW1234/Arcade.git" target="_blank" rel="noopener noreferrer"> GitHub Repository </a>
+                        <a className = "projectButton" href="https://github.com/MichelleW1234/Arcade/releases/download/v1.0.0/Arcade_1.0.0.dmg"> Download Arcade 1.0.0</a>
+                        <button className = "projectButton" onClick = {() => setArcadeMediaFlag(true)}> View Video Content </button>
+                    </div>
+
+                    
                 </div>
 
-                
+                <div className = "projectContainer">
+                    <h1 className = "projectTitle">
+                        TritonTurnUp
+                    </h1>
+                    <h1 className = "projectText">
+                        Description:
+                    </h1>
+                    <p className = "projectText">
+                        Our web app, TritonTurnup, allows UCSD students to browse a detailed list of upcoming and ongoing events 
+                        affiliated with the school. Upon logging in, students can add these events to their calendars based on their 
+                        schedule availability, promoting socialization and stress relief by raising awareness of fun and engaging events 
+                        within the Triton community.
+                    </p>
+                    <h1 className = "projectText">
+                        My Role:
+                    </h1>
+                    <p className = "projectText">
+                        - Contributed to UI/UX and front-end in an Agile team using GitHub <br/>
+                        - Conducted user research and testing to improve engagement <br/>
+                        - Mentored by professor with extensive software development experience <br/>
+
+                    </p>
+                    <h1 className = "projectText">
+                        Media:
+                    </h1>
+                    <div className = "projectMediaContainer">
+                        <a className = "projectButton" href="https://github.com/MichelleW1234/CSE-110-Project.git" target="_blank" rel="noopener noreferrer"> GitHub Repository </a>
+                        <a className = "projectButton" href="https://drive.google.com/file/d/1bjoVWfcJ89o7ha2sFHtFunMhr9dIxjtt/view" target="_blank" rel="noopener noreferrer"> Watch Demo Video </a>
+                    </div>
+                </div> 
             </div>
-
-            <div className = "projectContainer">
-                <h1 className = "projectTitle">
-                    TritonTurnUp
-                </h1>
-                <h1 className = "projectText">
-                    Description:
-                </h1>
-                <p className = "projectText">
-                    Our web app, TritonTurnup, allows UCSD students to browse a detailed list of upcoming and ongoing events 
-                    affiliated with the school. Upon logging in, students can add these events to their calendars based on their 
-                    schedule availability, promoting socialization and stress relief by raising awareness of fun and engaging events 
-                    within the Triton community.
-                </p>
-                <h1 className = "projectText">
-                    My Role:
-                </h1>
-                <p className = "projectText">
-                    - Worked with an Agile team to design and develop a web application managed on GitHub, under the mentorship of a professor with extensive software development expertise. <br/>
-                    - Contributed to the planning, design, and development phases, focusing primarily on the app's UI/UX and front-end. <br/>
-                    - Conducted user research and usability testing to gain insights into user needs, behaviors, and product interactions. <br/>
-                </p>
-                <h1 className = "projectText">
-                    Media:
-                </h1>
-                <div className = "projectMediaContainer">
-                    <a className = "projectButton" href="https://github.com/MichelleW1234/CSE-110-Project.git" target="_blank" rel="noopener noreferrer"> GitHub Repository </a>
-                    <a className = "projectButton" href="https://drive.google.com/file/d/1bjoVWfcJ89o7ha2sFHtFunMhr9dIxjtt/view" target="_blank" rel="noopener noreferrer"> Watch Demo Video </a>
-                </div>
-            </div> 
 
 
         </div>
