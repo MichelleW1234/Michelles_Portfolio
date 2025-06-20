8a67e1dc82d37f0e79a52564eb19730b82c61550
Michelle Wee <michellew1822@gmail.com>
2025-06-20 13:37:38 -0700

    mobile stuff

--- Michelles_Portfolio/src/components/Contact.jsx
+++ Michelles_Portfolio/src/components/Contact.jsx
@@ -8,7 +8,7 @@ function Contact() {
             Let's Connect!
         </h1>
         <div className = "contactContainer">
-          <a className = "contactBox"href="mailto: michellew1822@gmail.com"> Email Me</a>
+          <a className = "contactBox" href="mailto: michellew1822@gmail.com"> Email Me</a>
           <a className = "contactBox" href="https://www.linkedin.com/in/michelle-wee123/" target="_blank" rel="noopener noreferrer"> LinkedIn</a>
           <a className = "contactBox" href="https://github.com/MichelleW1234" target="_blank" rel="noopener noreferrer"> Github </a>
         </div>
