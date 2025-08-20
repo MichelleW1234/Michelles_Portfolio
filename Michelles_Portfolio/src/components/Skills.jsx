903b869161f46b4c3a4a06a90291c4f0ad84b205
Michelle Wee <michellew1822@gmail.com>
2025-08-20 15:42:36 -0700

    editing

--- Michelles_Portfolio/src/components/Skills.jsx
+++ Michelles_Portfolio/src/components/Skills.jsx
@@ -4,25 +4,19 @@ function Skills() {
 
   return (
     <div className = "skillsScreen">
-        <h1 className = "subTitle">
-            My Skills:
-        </h1>
-
-        <div className = "skillsContainer">
-          <ul>
-            <li> Programming Languages: Java, Python, C++, C </li>
-            <li> UI/UX: React (JavaScript), CSS, Figma </li>
-            <li> Web Application Development </li>
-            <li> Agile & Software Development</li>
-          </ul>
-
-          <ul>
-            <li> Problem-solving: Data Structures, Algorithms, Dynamic Programming</li>
-            <li> Machine Learning Algorithms </li>
-            <li> Discrete Mathematics, Linear Algebra, Calculus </li>
-            <li> Critical Thinking & Creativity </li>
-          </ul>
-        </div>
+      <h1 className = "subTitle">
+        My Skills:
+      </h1>
+      <div className = "skillsContainer">
+        Programming Languages: Java, Python, C++, C <br/> <br/>
+        Front-end: React (JavaScript), CSS, Figma <br/> <br/>
+        Web Application Development <br/> <br/>
+        Agile & Software Development <br/> <br/>
+        Prompt Engineering: Photo & Video Generation <br/> <br/>
+        Problem-solving: Data Structures, Algorithms, Dynamic Programming <br/> <br/>
+        Machine Learning Algorithms <br/> <br/>
+        Discrete Mathematics, Linear Algebra, Calculus
+      </div>
     </div>
 
   )
