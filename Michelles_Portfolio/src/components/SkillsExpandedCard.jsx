5962610d0f1d3eb6785ee49956b33dd589c87429
Michelle Wee <michellew1822@gmail.com>
2026-01-12 16:24:44 -0800

    skills section

--- Michelles_Portfolio/src/components/SkillsExpandedCard.jsx
+++ Michelles_Portfolio/src/components/SkillsExpandedCard.jsx
@@ -2,12 +2,22 @@ import "./SkillsExpandedCard.css";
 
 function SkillsExpandedCard({activeSkill, setActiveSkill}) {
 
+  const skillDescriptions = [" This is for React", 
+    " This is for CSS",
+    " This is for Figma",
+    " This is for Web application devlopment",
+    " This is for Agile & Software Development",
+    " This is for Game development"
+  ]
+
   return (
 
     <div className = "skillExpandedCardFlag">
         <div className="skillExpandedCard">
-            Testing
-            <button onClick = {() => setActiveSkill(-1)}>Close</button>
+          <p className="skillExpandedCardDescription"> 
+            {skillDescriptions[activeSkill]}
+          </p>
+          <button className = "skillExpandedCardButton" onClick = {() => setActiveSkill(-1)}>Close</button>
         </div>
     </div>
 
